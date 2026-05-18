9b1a1ee7ed57f08ae68c59723432e40bfd2f8894
Menna <menatullah.ibrahim@asset.com.eg>
2026-05-18 23:45:51 +0300

    section 12

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿
+using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+public record CreateProductCommand(string Name,List<string> Categories, string Description, string ImageFile ,decimal Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿
+
+using MediatR;
+
+namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand>
+    : IRequestHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand,  TRespose>
+    : IRequestHandler<TCommand, TRespose>
+    where TCommand : ICommand<TRespose>
+    where TRespose : notnull
+{
+}
